9f69db74d59a58b5988f58e876682502b99372ab
Huub van de Hoef <huub@beyondSports.nl>
2021-05-10 09:16:17 +0200

    Added shader project

---
+++ Assets/Scripts/GlowScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class GlowScript : MonoBehaviour
+{
+	[SerializeField]
+	private Material _material;
+
+	private void OnMouseOver()
+	{
+		_material.SetColor("_EmissionColor", new Color(1, 1, 1));
+	}
+
+	private void OnMouseExit()
+	{
+		_material.SetColor("_EmissionColor", new Color(1, 0, 0));
+	}
+}
---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+
+
+	// Update is called once per frame
+	void Update()
+	{
+		transform.Rotate(new Vector3(0, 0.5f, 0));
+	}
+}
